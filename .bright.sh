ea83eebc047d8f25019162c78d739aeeb06e1dec
Cristian Barbaro <cristiandanielbarbaro@gmail.com>
2015-10-24 16:35:21 -0300

    Added comments to use.

--- .bright.sh
+++ .bright.sh
@@ -1,4 +1,9 @@
 #! /bin/bash
 
-sudo chown root:castlerock /sys/class/backlight/intel_backlight/brightness
+# To add this script in start up, execute as superuser:
+# crontab -e
+# @reboot /$HOME/.bright.sh
+# Add permissions: chmod 755 /$HOME/.bright.sh
+
+sudo chown root:$USER /sys/class/backlight/intel_backlight/brightness
 sudo chmod 664 /sys/class/backlight/intel_backlight/brightness
