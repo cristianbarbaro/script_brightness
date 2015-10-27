834e60d9f6330ac4887247da08eae8a18aa04dde
Cristian Barbaro <cristiandanielbarbaro@gmail.com>
2015-10-27 14:13:02 -0300

    Fixed username. If execute as superuser, castlerock is root (obviously).

--- .start_bright.sh
+++ .start_bright.sh
@@ -2,11 +2,12 @@
 
 # To add this script in start up, execute as superuser:
 # crontab -e
-# @reboot /$HOME/.start_bright.sh
-# Add permissions: chmod 755 /$HOME/.start_bright.sh
+# @reboot /home/your_username/.start_bright.sh
+# Add permissions: chmod 755 .start_bright.sh
 # To remove of crontab, execute crontab -r. View man crontab.
+# Replace "your_username" for your user.
 
 brightness='/sys/class/backlight/intel_backlight/brightness'
 
-sudo chown root:$USER $brightness
+sudo chown root:your_username $brightness
 sudo chmod 664 $brightness
